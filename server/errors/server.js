e657a9abe9833b767aceeee1e4e65abbe93ab7ef
Arthur Guiot <arguiot@gmail.com>
2019-04-27 09:32:03 +0800

    Basic API

--- server/errors/server.js
+++ server/errors/server.js
@@ -1,114 +1,96 @@
-const mongoose = require('mongoose');
+const jsondb = require('node-json-db');
 
-// Connection URL
-const url = 'mongodb+srv://arguiot:zihty6-korxak-weMjab@studioeditorjserror-jmnlo.gcp.mongodb.net/editorErrorJS';
+const db = new jsondb("editorErrorJS", true, true);
 
-// Database Name
-const dbName = 'editorErrorJS';
+const model = {
+	name: String,
+	recurrence: Number,
+	json: {
+		err: String,
+		src: String,
+		line: Number,
+		column: Number,
+		time: String,
+		userAgent: String
+	}
+}
 
-mongoose.connect(url, {
-	poolSize: 2,
-	ssl: true,
-	keepAlive: 300000,
-	connectTimeoutMS: 30000,
-	autoReconnect: true,
-	reconnectTries: 30000000,
-	reconnectInterval: 5000,
-	promiseLibrary: global.Promise,
-	useNewUrlParser: true
-});
+function logRecords(callback, res) {
+	try {
+		const data = db.getData("/db");
+		callback(data)
+	} catch (error) {
+		// The error will tell you where the DataPath stopped. In this case test1
+		// Since /test1/test does't exist.
+		console.error(error);
+		res.end(error)
+	};
+}
 
-const db = mongoose.connection;
+/** Server */
 
-db.on('error', console.error.bind(console, 'connection error:'));
+const express = require("express")
+const app = express()
+const port = 8080
 
-db.once('open', function() {
-	const model = new mongoose.Schema({
-		name: String,
-		recurrence: Number,
-		json: {
-			err: String,
-			src: String,
-			line: Number,
-			column: Number,
-			time: String,
-			userAgent: String
+app.use(express.json()); // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
+app.get("/", (req, res) => {
+	logRecords(records => {
+		let out = "<h1>StudIO - Editor common errors</h1><br>"
+		for (let record of records) {
+			out += `Name: ${record.name}<br>`
+			out += `Number: ${record.recurrence}<br><hr>`
 		}
-	})
+		res.send(out)
+		res.end()
+	}, res)
+})
 
-	const editorErrorJS = mongoose.model("editorErrorJS", model)
+app.post("/", (req, res) => {
+	const name = req.body.name
+	const json = req.body["JSON"]
 
-
-	function logRecords(callback) {
-		editorErrorJS.find((err, records) => {
-			if (err) return console.error(err);
-			callback(records)
-		})
+	const record = {
+		name: name,
+		recurrence: 1,
+		json: {
+			err: json.err,
+			src: json.src,
+			line: json.line,
+			column: json.column,
+			time: json.time,
+			userAgent: json.userAgent
+		}
 	}
 
-	/** Server */
-
-	const express = require("express")
-	const app = express()
-	const port = 8080
-
-	app.use(express.json()); // to support JSON-encoded bodies
-	app.use(express.urlencoded()); // to support URL-encoded bodies
-
-	app.get("/", (req, res) => {
-		logRecords(records => {
-			let out = "<h1>StudIO - Editor common errors</h1><br>"
-			for (let record of records) {
-				out += `Name: ${record.name}<br>`
-				out += `Number: ${record.recurrence}<br><hr>`
+	const query = {
+		name: name
+	}
+	logRecords(records => {
+		records.forEach((r, i) => {
+			if (r.name == name) {
+				record.recurrence = r.recurrence + 1
+				db.delete(`/db[${i}]`)
 			}
-			res.send(out)
-			res.end()
 		})
+	}, res)
+	db.push(`/db[]`, record)
+
+	res.jsonp({
+		error: "success"
 	})
+	res.end()
+})
 
-	app.post("/", (req, res) => {
-		const name = req.body.name
-		const json = req.body["JSON"]
+app.delete("/", (req, res) => {
+	const i = req.body.id
 
-		const record = new editorErrorJS({
-			name: name,
-			recurrence: 1,
-			json: {
-				err: json.err,
-				src: json.src,
-				line: json.line,
-				column: json.column,
-				time: json.time,
-				userAgent: json.userAgent
-			}
-		})
+	db.delete(`/db[${i}]`)
 
-		const query = { name: name }
-		editorErrorJS.findOne(query, (err, r) => {
-			if (r != null && err == null) {
-				editorErrorJS.updateOne(query, { recurrence: r.recurrence + 1 }, (err, r) => {
-					res.jsonp({
-						error: "success",
-						response: r.toString()
-					})
-					res.end()
-				})
-			} else {
-				record.save(err => {
-					if (err) {
-						res.jsonp({
-							error: err
-						})
-						res.end()
-					}
-					res.jsonp({
-						error: "success"
-					})
-					res.end()
-				})
-			}
-		})
-	})
-	app.listen(process.env.PORT || port, () => console.log(`App listening on port ${port}!`))
-});
+	res.end(0)
+})
+
+
+app.listen(process.env.PORT || port, () => console.log(`App listening on port ${port}!`))
